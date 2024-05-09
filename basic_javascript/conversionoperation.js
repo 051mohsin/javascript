0f1ca08ce087ca1024ba435d58f07da064d5aa68
Mohsin Riaz <107985569+051mohsin@users.noreply.github.com>
2024-05-09 16:23:34 +0000

    conversionoperation

---
+++ basic_javascript/conversionoperation.js
@@ -0,0 +1,22 @@
+// conversion operation
+
+
+let number = 24;
+
+
+
+let name ="mohsin"
+
+console.log(typeof age)
+
+
+console.log(typeof name)
+
+
+let valueInnumber = Number(number)
+
+console.log('conversion operation', valueInnumber);
+
+
+
+
