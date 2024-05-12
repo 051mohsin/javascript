4ca5149c79dbb433bb0082a0e49c3d3afd644106
Mohsin Riaz <107985569+051mohsin@users.noreply.github.com>
2024-05-12 06:48:48 +0000

    datatype_summary

---
+++ basic_javascript/datatype_summary.js
@@ -0,0 +1,11 @@
+// Two Type of datatype
+//1 primitive
+//2 Reference (Non Primitive)
+
+// Primitive
+
+// 7, String , Number, Boolean, Null ,undfined , Symbol ,BigInt
+
+// Reference (non primitive)
+
+// Array , Object , Function
