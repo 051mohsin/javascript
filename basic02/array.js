6b48dd0d5a4da15a45e2044f0798b99a198c5050
Mohsin Riaz <107985569+051mohsin@users.noreply.github.com>
2024-06-04 04:42:15 +0000

    array and method add

--- basic02/array.js
+++ basic02/array.js
@@ -1,11 +1,17 @@
 const myArr = [0,1,2,3,4,5]
 
-const myArr2 = new Array(1,2,3,4,5,6)
-console.log(myArr2)
+// const myArr2 = new Array(1,2,3,4,5,6)
+
+// console.log(myArr2)
 
 // Array Methods
-
 // myArr.push(6,7)
 // myArr.pop()
 // console.log(myArr)
+  
 
+// myArr.unshift(9)
+
+
+myArr.shift()
+console.log(myArr)
