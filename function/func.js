449b105a8ea644a65af8bc865df74165d2fd0f74
051mohsin <mohsinriaz051@gmail>
2024-07-01 21:57:33 +0500

    function and parametes

---
+++ function/func.js
@@ -0,0 +1,16 @@
+function func(a,b){
+
+    let result = a+b
+
+    return result
+
+    // return a+b
+
+}
+//  console.log(func(2,2));
+
+const results = func(2,5);
+
+console.log("results",results);
+
+
