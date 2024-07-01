119c70f0c77f9757469bad88ca68ef525d7b1ec8
051mohsin <mohsinriaz051@gmail>
2024-07-01 19:00:36 +0500

    nasted object and sperad methd

--- basic02/object.js
+++ basic02/object.js
@@ -28,8 +28,47 @@ Object.freeze(userObject)
 
 userObject.name="mohisn"
 
-console.log(userObject)
+// console.log(userObject)
 
+// nasted object 
+
+const obj1 = { 
+
+   user:[
+        {
+            user:"mohsin",
+            email:"mohsin1@gamil.com"
+        },
+        {
+            user : "ali0",
+            email :"ali0@gamil.com"
+        },
+        {
+            user : "ali1",
+            email :"ali1@gamil.com"
+        },
+        {
+            user : "ali2",
+            email :"ali2@gamil.com"
+        }
+    ]
+}
+
+// console.log(obj1.user[2]);
+
+// combine object 
+
+const obj2 = {1:"a", 2 : "b" , 3:"c"}
+
+const obj3 = {4 : "a", 5 : "b" , 6:"c"}
+
+const obj4 =Object.assign(obj2,obj3)
+
+const obj5 = {...obj2,...obj3}
+
+// console.log(`object combine by sperad method `, {obj5});
+
+console.log('object combine by sperad method', obj5);
 
 
 
