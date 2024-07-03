c05417a53cd23fd13667e479cbfcd6b9c012e501
Mohsin Riaz <107985569+051mohsin@users.noreply.github.com>
2024-07-03 10:02:57 +0000

    nested function

--- function/func.js
+++ function/func.js
@@ -11,6 +11,22 @@ function func(a,b){
 
 const results = func(2,5);
 
-console.log("results",results);
+// console.log("results",results);
+
+function myFunc(theArr) {
+    theArr[0] = 30;
+  }
+const arr = [45]
+// console.log(arr[0]);
+myFunc(arr)
+  // console.log(arr[0]);
 
 
+  const func1  = function  func2(a,b){
+   function fun3()
+    {
+    return a+ b
+    }
+   return fun3();
+  }
+console.log(func1(2,3));
