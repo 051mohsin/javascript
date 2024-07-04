8877fe197e9ea0bce5a96dad40d8129cea1e6f36
051mohsin <mohsinriaz051@gmail>
2024-07-04 20:58:05 +0500

    object with function

--- function/func.js
+++ function/func.js
@@ -1,6 +1,6 @@
-function func(a,b){
+function func(a, b) {
 
-    let result = a+b
+    let result = a + b
 
     return result
 
@@ -9,19 +9,32 @@ function func(a,b){
 }
 //  console.log(func(2,2));
 
-const results = func(2,5);
+const results = func(2, 5);
 
 // console.log("results",results);
 
 
-function logginMessage(massges){
+function logginMessage(massges) {
 
     return `${massges},just loggin`
-} 
+}
 
 const massageto = logginMessage("mohsin")
 
-console.log(massageto)
+// console.log(massageto)
+
+const fun_ = function fun1(a, b) {
+
+    function fun2() {
+
+        return a + b
+    }
+    return fun2()
+
+}
+const fun3 = fun_(2,5)
+
+console.log(fun3)
 
 
 
