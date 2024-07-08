24288a95ed35c51841bcef248069893493ccac43
051mohsin <mohsinriaz051@gmail.com>
2024-07-08 11:35:03 +0500

    main

--- README.md
+++
@@ -1,2 +0,0 @@
-# javascript
- A code repo of  javascript series
--- basic_javascript/variable.js
+++
@@ -1,8 +0,0 @@
-const accountId= 1123566;
-let accountEmail="xyz@gmail.com";
-var accountPassoword="1446RXZ";
-accountCity="lahore";
-
-// accountId=2;  not allowed
-
-console.log(accountId);
